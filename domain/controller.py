fd99e5b7df6098b1e40ad78e2aeabd88e58df0a4
Kris <nataza7394@gmail.com>
2024-03-02 15:49:30 +0300

    some updates in libraries and app is got more stable

--- domain/controller.py
+++ domain/controller.py
@@ -14,17 +14,28 @@ class Controller:
 
     async def execute_task(self, task) -> None:
         if task["action"] == "parse":
-            pars = Parser(task)
-            await pars.parse()
-            logger.info(f"Parsing finished / {task['title']}")
+            try:
+                pars = Parser(task)
+                await pars.parse()
+                logger.info(f"Parsing finished / {task['title']}")
+            except Exception as e:
+                logger.error(f'parsing error / {e}')
         elif task["action"] == "translate":
-            trans = TrManager(task["title"], task["language"], task["config"])
-            await asyncio.sleep(3)
-            await trans.translate()
-            logger.info(f"Translating task is created / {task['title']}")
+            try:
+                trans = TrManager(task["title"],
+                                  task["language"],
+                                  task["config"])
+                await asyncio.sleep(3)
+                await trans.translate()
+                logger.info(f"Translating task is created / {task['title']}")
+            except Exception as e:
+                logger.error(f'translating error / {e}')
         elif task["action"] == "save":
-            fileManeger = FileManager(task)
-            await fileManeger.save()
+            try:
+                fileManeger = FileManager(task)
+                await fileManeger.save()
+            except Exception as e:
+                logger.error(f'saving error / {e}')
 
     async def run(self) -> None:
         tasks = [self.execute_task(task) for task in self.tasks]
